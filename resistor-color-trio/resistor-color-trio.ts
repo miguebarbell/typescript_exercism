9bc92229b1c10d9548a20c348a9121bb449b5ca3
Miguel <contact@debloat.us>
2022-03-04 12:32:07 -0500

    leap year begin

--- resistor-color-trio/resistor-color-trio.ts
+++ resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,26 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedResistorValue(bands: [string, string, string]): string {
+  const Bands = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+  ]
+  const sum = `${Bands.indexOf(bands[0])}${Bands.indexOf(bands[1])}`
+  const calculateOhms = (value: string): string=> {
+    let ohms='';
+    for (let i = 0; i < Bands.indexOf(bands[2]); i++) {
+      ohms = ohms + '0'
+    }
+    return ohms
+  }
+
+const totalOhms = `${sum}${calculateOhms(bands[2])}`
+if (totalOhms.length > 3) return `${totalOhms.slice(0,-3)} kiloohms`
+return `${totalOhms} ohms`
 }
